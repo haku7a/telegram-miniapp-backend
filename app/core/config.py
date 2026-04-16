8524dfc132971f52e809b872f39cf4658488c1ef
haku7a <v.a.neyaskin@gmail.com>
2026-04-16 04:57:26 +0300

    refactor: use SecretStr for sensitive configuration values

--- app/core/config.py
+++ app/core/config.py
@@ -1,19 +1,20 @@
+from pydantic import SecretStr
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
+    POSTGRES_PASSWORD: SecretStr
     POSTGRES_DB: str
 
     DB_HOST: str
     DB_PORT: int
 
-    BOT_TOKEN: str
+    BOT_TOKEN: SecretStr
 
     @property
     def database_url(self) -> str:
-        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}"
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD.get_secret_value()}@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}"
 
     model_config = {"env_file": ".env", "extra": "ignore"}
 
